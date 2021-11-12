9256c2d5020ef99bb77abc2b13ad7af95649e085
Yuuto Chinen <e215733@ie.u-ryukyu.ac.jp>
2021-11-12 15:13:04 +0900

    ステップ３

--- app/src/main/java/jp/ac/uryukyu/ie/e215733/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215733/Hero.java
@@ -1,5 +1,7 @@
 package jp.ac.uryukyu.ie.e215733;
 
+import javax.print.event.PrintServiceAttributeEvent;
+
 /**
  * ヒーロークラス。
  *  String name; //敵の名前
@@ -9,10 +11,10 @@ package jp.ac.uryukyu.ie.e215733;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private  String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -28,6 +30,17 @@ public class Hero {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public Boolean getDead(){
+        return this.dead;
+    }
+
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
     /**
      * Enemyへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
@@ -35,7 +48,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
--- app/src/test/java/jp/ac/uryukyu/ie/e215733/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e215733/EnemyTest.java
@@ -19,6 +19,6 @@ void attackTest() {
         demoHero.attack(slime); // over kill
         }
     slime.attack(demoHero);
-    assertEquals(defaultHeroHp, demoHero.hitPoint);
+    assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
 }
